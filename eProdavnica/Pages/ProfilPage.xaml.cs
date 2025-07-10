464628651ce04fa77550a57f8831514e5afd871a
Samedin Tutnjic <samedin.tutnjic.23@size.ba>
2025-07-10 05:39:34 +0200

    All

---
+++ eProdavnica/Pages/ProfilPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace eProdavnica.Pages
+{
+    public partial class ProfilPage : ContentPage
+    {
+        public ProfilPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
